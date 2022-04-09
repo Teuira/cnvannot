2e0817e8c3a9707debffb0493a8fd364f6f5b298
Teuira <39984272+Teuira@users.noreply.github.com>
2022-04-09 18:32:48 +0200

    refactor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# Annotateur - backend
+# Annotator - backend
 
 import sys
-from intervaltree import IntervalTree
+from DGV import dgv_load
+from RefSeq import refseq_load
 
 print("Annotator")
 query = sys.argv[1]
@@ -11,49 +12,15 @@ query_start = int(query_start_end[0])
 query_end = int(query_start_end[1])
 query_type = query.split(':')[2]
 
-# DGV DB
+dgv_db = dgv_load()
+refseq_db = refseq_load()
 
-chrDict = {}
-
-dgv_base_path = 'data/DGV.GS.March2016.50percent.GainLossSep.Final.hg19.gff3'
-with open(dgv_base_path) as f:
-    for line in f:
-        parts = line.split('\t')
-        chrom = parts[0]
-        start = int(parts[3])
-        stop = int(parts[4])
-        dat = parts[8]
-        dat_parts = dat.split(';')
-        var_type = ''
-        freq_percent = ''
-        for dp in dat_parts:
-            if dp.startswith('variant_sub_type'):
-                var_type = dp.split('=')[1]
-            if dp.startswith('Frequency'):
-                freq_percent = float(dp.split('=')[1][:-1])
-        if var_type == 'Gain':
-            var_type = 'DUP'
-        elif var_type == 'Loss':
-            var_type = 'DEL'
-        if var_type == '':
-            print("Cannot parse line")
-
-        if chrom not in chrDict:
-            # Add new interval tree as value
-            chrDict[chrom] = IntervalTree()
-
-        try:
-            chrDict[chrom][start:stop] = {'chr': chrom, 'start': start, 'stop': stop,
-                                          'var_type': var_type, 'freq': freq_percent}
-        except ValueError:
-            pass
-
-if query_chr in chrDict:
-    if chrDict[query_chr].overlaps(query_start, query_end):
+if query_chr in dgv_db:
+    if dgv_db[query_chr].overlaps(query_start, query_end):
         print("Overlap found in DGV")
         with open("out.json", "a") as out_file:
             out_file.write('{')
-        for r in chrDict[query_chr][query_start:query_end]:
+        for r in dgv_db[query_chr][query_start:query_end]:
             t = r.data['var_type']
             if t != query_type:
                 continue
