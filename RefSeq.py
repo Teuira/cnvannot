6693700333ba5ca4948630416de73461499f69ac
Davide C <cdvd87@yahoo.it>
2022-04-10 10:33:20 +0200

    save

--- RefSeq.py
+++ RefSeq.py
@@ -1,7 +1,7 @@
 from intervaltree import IntervalTree
 
 
-# DGV DB
+# RefSeq DB
 
 def refseq_load():
     chr_dict = {}
@@ -9,4 +9,21 @@ def refseq_load():
     with open(dgv_base_path) as f:
         for line in f:
             parts = line.split('\t')
+            chrom = parts[2]
+            start = int(parts[4])
+            stop = int(parts[5])
+            name1 = parts[1].lower()
+            name2 = parts[12].lower()
+
+            if chrom not in chr_dict:
+                # Add new interval tree as value
+                chr_dict[chrom] = IntervalTree()
             pass
+
+    try:
+        chr_dict[chrom][start:stop] = {'chr': chrom, 'start': start, 'stop': stop,
+                                       'name1': name1, 'name2': name2}
+    except ValueError:
+        pass
+
+    return chr_dict
