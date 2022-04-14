1f60d52d591ca3868c0550f2d7d72573b2e9f460
Davide <cdvd87@yahoo.it>
2022-04-14 09:44:09 +0000

    Serialization for OMIM is OK. OMIM is OK.

--- src/omim.py
+++ src/omim.py
@@ -1,20 +1,18 @@
 import os.path
-import pickle
+
+from common import Common
+from serialization import *
 
 
 # OMIM DB
 
 def omim_morbid_genes_load():
     gene_dict = {}
-    omim_mg_base_dir = 'data'
     omim_mg_base_file = '20181126_morbidGenes.tsv'
-    omim_mg_base_path = os.path.join(omim_mg_base_dir, omim_mg_base_file)
+    omim_mg_base_path = os.path.join(Common.data_path, omim_mg_base_file)
 
-    serialized_path = os.path.join('serialized', omim_mg_base_file)
-    if os.path.isfile(serialized_path):
-        with open(os.path.join('serialized', omim_mg_base_file), 'rb') as f1:
-            gene_dict = pickle.load(f1)
-            return gene_dict
+    if serialization_is_serialized(omim_mg_base_file):
+        gene_dict = serialization_deserialize(omim_mg_base_file)
 
     with open(omim_mg_base_path) as f:
         for line in f:
@@ -23,7 +21,6 @@ def omim_morbid_genes_load():
             if parts[0] not in gene_dict:
                 gene_dict[gene] = {"morbid": parts[1]}
 
-    with open(os.path.join('serialized', omim_mg_base_file), 'wb') as f1:
-        pickle.dump(gene_dict, f1)
+    serialization_serialize(gene_dict, omim_mg_base_file)
 
     return gene_dict
