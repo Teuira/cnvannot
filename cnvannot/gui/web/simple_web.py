4f6cc6df1acf91aaa46c8ffb4b8af341316ca618
Davide <cdvd87@yahoo.it>
2022-04-18 21:45:52 +0000

    Basic web template works.

--- cnvannot/gui/web/simple_web.py
+++ cnvannot/gui/web/simple_web.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main_page():
-    return "OK"
+    return render_template('home.html')
 
 
 def run_server():
