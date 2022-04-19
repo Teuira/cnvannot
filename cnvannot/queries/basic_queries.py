5f4c375dc554c5420c1d2d8a6052752a226365c9
Davide <cdvd87@yahoo.it>
2022-04-19 09:22:28 +0000

    Queries OK.

--- cnvannot/queries/basic_queries.py
+++ cnvannot/queries/basic_queries.py
@@ -10,3 +10,14 @@ def query_overlaps(db, query: GenomicCoordinates) -> bool:
             return True
 
     return False
+
+
+def query_overlap_count(db, query: GenomicCoordinates) -> int:
+    if not query_overlaps(db, query):
+        return 0
+    count = 0
+    if query.chr in db:
+        itree: IntervalTree = db[query.chr]
+        count = len(itree[query.start:query.end])
+
+    return count
---
+++ cnvannot/queries/specific_queries.py
@@ -0,0 +1,15 @@
+from cnvannot.common.coordinates import GenomicCoordinates
+
+
+def dgv_gold_overlap_count_1_percent(db, query: GenomicCoordinates) -> int:
+    res = 0
+    if query.chr in db:
+        if db[query.chr].overlaps(query.start, query.end):
+            for r in db[query.chr][query.start:query.end]:
+                t = r.data['var_type']
+                if t != query.type:
+                    continue
+                if r.data['freq'] >= 1:
+                    res = res + 1
+
+    return res
