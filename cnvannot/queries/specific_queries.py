a61db3266f35a5d4f409aed5955dc33351f7f2de
Davide <cdvd87@yahoo.it>
2022-04-28 15:38:36 +0000

    Added overlap size calculation for 2 specific already existent queries and for a new query.

--- cnvannot/queries/specific_queries.py
+++ cnvannot/queries/specific_queries.py
@@ -1,4 +1,5 @@
 from cnvannot.common.coordinates import GenomicCoordinates
+from cnvannot.queries.basic_queries import overlap_size
 
 
 def dgv_gold_overlap_count_1_percent(db, query: GenomicCoordinates) -> int:
@@ -10,7 +11,8 @@ def dgv_gold_overlap_count_1_percent(db, query: GenomicCoordinates) -> int:
                 if t != query.type:
                     continue
                 if r.data['freq'] >= 1:
-                    res = res + 1
+                    if overlap_size(r.begin, query.start, r.end, query.end) >= 0.7 * (query.end - query.start):
+                        res = res + 1
 
     return res
 
@@ -21,6 +23,17 @@ def is_france_incomplete_penetrance(db, query: GenomicCoordinates) -> (bool, str
             for r in db[query.chr][query.start:query.end]:
                 t = r.data['var_type']
                 if t == query.type:
-                    return True, r.data['desc']
+                    if overlap_size(r.begin, query.start, r.end, query.end) >= 0.7 * (query.end - query.start):
+                        return True, r.data['desc']
 
     return False, ''
+
+
+def exc_overlaps_70_percent(db, query: GenomicCoordinates) -> bool:
+    if query.chr in db:
+        if db[query.chr].overlaps(query.start, query.end):
+            for r in db[query.chr][query.start:query.end]:
+                if overlap_size(r.begin, query.start, r.end, query.end) >= 0.7 * (query.end - query.start):
+                    return True
+
+    return False
