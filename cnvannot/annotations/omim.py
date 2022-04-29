93add77a3f86c6aa83b223b356203b71f2b67718
Davide <cdvd87@yahoo.it>
2022-04-29 09:19:53 +0000

    Omim works.

--- cnvannot/annotations/omim.py
+++ cnvannot/annotations/omim.py
@@ -1,31 +1,56 @@
 import os.path
 
+from intervaltree import IntervalTree
 from cnvannot.common.serialization import *
 
 
 # OMIM DB
 
 def omim_morbid_genes_load():
-    gene_dict = {}
+    hgnc_dict = {}
 
-    omim_mg_base_file = '20181126_morbidGenes.tsv'
-    omim_mg_base_path = os.path.join(Common.data_path, omim_mg_base_file)
+    chr_dict = {}  # final
 
-    if serialization_is_serialized(omim_mg_base_file):
-        return serialization_deserialize(omim_mg_base_file)
+    refseq_base_file = 'refGene.txt'
+    refseq_base_path = os.path.join(Common.data_path, refseq_base_file)
 
-    with open(omim_mg_base_path) as f:
+    hgnc_base_file = 'hgnc_complete_set_2022-04-01.tsv'
+    hgnc_base_path = os.path.join(Common.data_path, hgnc_base_file)
+
+    final_base = 'coord2genes'
+
+    if serialization_is_serialized(final_base):
+        return serialization_deserialize(final_base)
+
+    with open(hgnc_base_path) as f:
+        for line in f:
+            if line.startswith('hgnc_id'):
+                continue
+            parts = line.split('\t')
+            omim_id = parts[31]
+            if omim_id == '':
+                continue
+            refseq_id = parts[23]
+            hgnc_dict[refseq_id] = {'gene_aliases': parts[1] + '|' + parts[8]}
+
+    with open(refseq_base_path) as f:
         for line in f:
             parts = line.split('\t')
-            gene = parts[0]
-            if parts[0] not in gene_dict:
-                gene_dict[gene] = {"morbid": parts[1]}
-                # TODO: IMPLEMENT
+            refseq_id = parts[1]
+            chrom = parts[2]
+            start = int(parts[4])
+            stop = int(parts[5])
+            if refseq_id in hgnc_dict:
+                # refSeq id matches with OMIM gene.
+                if chrom not in chr_dict:
+                    # Add new interval tree as value
+                    chr_dict[chrom] = IntervalTree()
+                try:
+                    chr_dict[chrom][start:stop] = {'chr': chrom, 'start': start, 'stop': stop,
+                                                   'omim_gene_aliases': hgnc_dict[refseq_id]['gene_aliases']}
+                except ValueError:
+                    pass
 
-    serialization_serialize(gene_dict, omim_mg_base_file)
+    serialization_serialize(chr_dict, final_base)
 
-    return gene_dict
-
-
-def omim_query(refseq_db, query):
-    pass
+    return chr_dict
